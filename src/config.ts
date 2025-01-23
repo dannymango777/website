99fdcb1571cc0e270fb6e030c6c55c4217fc3b1a
Daniel Mangasarian <156043585+dannymango777@users.noreply.github.com>
2025-01-23 10:24:51 -0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,21 @@
 export const siteConfig = {
-  title: "Eric Minassian",
+  title: "Daniel Mangasarian",
   description:
-    "SDE Intern at Amazon. Previously at Walmart. Studying CS + Business Administration at the University of California, Irvine.",
-  author: "Eric Minassian",
-  url: "https://www.ericminassian.com",
+    "Studying General Biology at the University of California, San Deigo.",
+  author: "Daniel Mangasarian",
+  url: "https://danielmangasarian.com",
   themeColor: "#111010",
 } as const;
 
 export const personalConfig = {
-  name: "eric",
-  emoji: "👋",
+  name: "daniel",
+  emoji: "🤠",
   description: `
-  I currently work as a Software Development Engineer at [Amazon](https://amazon.com),
-  where I work on [Project Kuiper](https://www.aboutamazon.com/what-we-do/devices-services/project-kuiper).
-  
-  Previously, I worked at [Walmart Global Tech](https://tech.walmart.com) as a software engineer II. 
-  
-  I'm currently studying Computer Science & Business Administration at [University of California, Irvine](https://uci.edu).`,
+  I'm currently studying General Biology at [University of California, San Diego](https://ucsd.edu).`,
   externalLinks: [
-    { href: "https://linkedin.com/in/minassian-eric", text: "linkedin" },
-    { href: "/Eric_Minassian_resume.pdf", text: "resume" },
-    { href: "https://github.com/eric-minassian", text: "github" },
-    { href: "mailto:eric@ericminassian.com", text: "email" },
+    { href: "https://www.linkedin.com/in/danielmangasarian/", text: "linkedin" },
+    { href: "", text: "resume" },
+    { href: "https://github.com/dannymango777", text: "github" },
+    { href: "mailto:danielmangasarian@gmail.com", text: "email" },
   ],
 } as const;
